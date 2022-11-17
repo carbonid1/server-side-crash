fd1e6dae1de1c40746ac19309b5a6ddfc5b6126d
Andrew Korin <carbonid1@gmail.com>
2022-11-17 18:01:24 +0200

    qc

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['page.tsx'],
+  pageExtensions: ['page.tsx', 'page.js'],
 }
 
 module.exports = nextConfig
---
+++ pages/5001.page.tsx
@@ -0,0 +1,4 @@
+export default function Custom500(props) {
+  console.log(props)
+  return <h1>500 - Server-side error occurred</h1>
+}
